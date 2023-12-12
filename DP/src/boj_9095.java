82537d962fd4ed963479b7a589d92cb7a03aa40c
pkh0106 <rudgml742@gmail.com>
2023-12-12 18:04:06 +0900

    Solved - 9095

---
+++ DP/src/boj_9095.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class boj_9095 {
+	static int dp[]= new int[12];
+	
+	public static int calc(int n) {
+		if(n < 4 || dp[n] > 0) return dp[n];
+		dp[n] = calc(n-1) + calc(n-2)+ calc(n-3);
+		return dp[n];
+	}
+	
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		
+		int n;
+		
+		dp[1]=1;
+		dp[2]=2;
+		dp[3]=4;
+		for(int i = 0; i<tc; i++) {
+			n = Integer.parseInt(br.readLine());
+			
+			System.out.print(calc(n));
+		}
+	}
+}
