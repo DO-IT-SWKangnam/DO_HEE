e36218658c32f0b49ec309c3a7ed140d12b430eb
pkh0106 <rudgml742@gmail.com>
2023-12-18 21:53:54 +0900

    Solved - 2751

---
+++ ETC/src/boj_2751.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class boj_2751 {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < n ;i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+		    sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
+
+/* sout를 쓰니까 계속 시간 초과가 나왔는데 StringBuilder를 쓰니까 괜찮았다,, */
