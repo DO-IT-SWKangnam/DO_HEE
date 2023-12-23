b31e0f140a461bf187ecf376e5198969175c0ddf
pkh0106 <rudgml742@gmail.com>
2023-12-23 20:11:15 +0900

    Solved - 10808

---
+++ ETC/src/boj_10808.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10808 {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		int arr[] = new int[26];
+		
+		for(int i = 0; i < str.length() ; i++) {
+			arr[str.charAt(i)-'a']++;
+		}
+		
+		for(int j:arr) {
+			System.out.print(j + " ");
+		}
+	}
+}
