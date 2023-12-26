04b2b04a347280828f1ac6e1189d2e8175f40692
pkh0106 <rudgml742@gmail.com>
2023-12-26 15:38:06 +0900

    Solved - 1934

---
+++ ETC/src/boj_1934.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1934 {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < tc ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int mul = a*b;
+			
+			while (b != 0) {
+				int r = a % b;
+				a = b;
+				b = r;
+			}
+
+			System.out.println(mul/a);
+		}
+	}
+}
